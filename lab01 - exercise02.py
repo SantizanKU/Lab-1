84ce05ca1c66f39f84b00cdeec8467e6f05f8f87
SantizanKU <santizan@ku.edu>
2025-08-29 16:57:55 -0500

    Create lab01 - exercise02.py

---
+++ lab01 - exercise02.py
@@ -0,0 +1,9 @@
+'''
+Author: Zane Santiago
+KUID: 3186478
+Date: 8-29-2025
+Lab: Lab01
+Last modified: 8-29-2025
+Purpose: Print biographical information
+'''
+print('My name is Zane Santiago\nI am an Electrical Engineering major.\nMy Hobbies are:\nGaming\nMusic\nCooking\nGoodbye')
